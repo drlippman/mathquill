e4fcfe261c7cebe7f73d944b44b5fb06d8bd9ccd
Daniel He <53242193+danieldanhe@users.noreply.github.com>
2026-03-26 16:07:53 +0800

    reflow on superscript/subscript change (and some typescript)

--- src/commands/math/commands.ts
+++ src/commands/math/commands.ts
@@ -362,8 +362,11 @@ class SupSub extends MathCommand {
           var src = this[supsub],
             dest = thisDir[supsub];
           if (!src) continue;
-          if (!dest) thisDir.addBlock(src.disown());
-          else if (!src.isEmpty()) {
+          if (!dest) {
+            thisDir.addBlock(src.disown());
+            src.blur(cursor);
+            thisDir.reflow();
+          } else if (!src.isEmpty()) {
             // ins src children at -dir end of dest
             src
               .domFrag()
@@ -530,6 +533,7 @@ class SupSub extends MathCommand {
             cmd.domFrag().addClass('mq-sup-only').children().last().remove();
           }
           this.remove();
+          cmd.reflow();
         };
       })(
         this,
